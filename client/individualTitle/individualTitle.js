0d20d0710d5a3b7914575d2bbeb34e738a98613f
Philip Chan <philch4n@gmail.com>
2016-02-25 12:01:59 -0600

    review button working

--- client/individualTitle/individualTitle.js
+++ client/individualTitle/individualTitle.js
@@ -4,4 +4,6 @@ app.controller('IndivController', function($scope, $location, Globals, Factory){
 	$scope.work = Globals.returnIndiv()
 	console.log('moved to indivController')
 
+	$scope.toggleBeans = false;
+
 })
