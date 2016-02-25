4215a7155eb16d4186c439dc8684e7c90e108891
Philip Chan <philch4n@gmail.com>
2016-02-25 12:30:31 -0600

    set up reviews

--- client/individualTitle/individualTitle.js
+++ client/individualTitle/individualTitle.js
@@ -5,5 +5,17 @@ app.controller('IndivController', function($scope, $location, Globals, Factory){
 	console.log('moved to indivController')
 
 	$scope.toggleBeans = false;
+	$scope.booBeans = false;
+	$scope.writeTitle;
+	$scope.writeBody;
+	$scope.allReviews;
 
+	$scope.postReview = function(){
+
+	}
+	$scope.getReview = function(){
+		//request will return array of object rows
+	}
+
+	$scope.getReview();
 })
